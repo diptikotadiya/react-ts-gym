3411ae0040452cd975dec200199874f5686e88e2
diptikotadiya <151667330+diptikotadiya@users.noreply.github.com>
2025-05-07 10:31:15 +0530

    Update WhatsApp.tsx

--- src/components/WhatsApp.tsx
+++ src/components/WhatsApp.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+//import React from 'react'
 import { BiLogoWhatsappSquare } from "react-icons/bi";
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 function WhatsApp() {
   return (
