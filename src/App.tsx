2f08655c79df35fd530d1ddc42feb435f7d842d6
diptikotadiya <151667330+diptikotadiya@users.noreply.github.com>
2025-05-07 10:32:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 import  Header  from './components/Header'
 import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
-import WhatsApp from './components/WhatsApp'
-import { Link } from 'react-router-dom'
+//import WhatsApp from './components/WhatsApp'
+//import { Link } from 'react-router-dom'
 
 function App() {
   
